364d8a36c9ec025934807a1c324ab99ff20c7244
dz-1010 <qwop_01@naver.com>
2025-09-25 02:25:59 +0900

    design: 비밀번호 변경 버튼 CSS 수정

--- dajeong/login-project/src/pages/Home.jsx
+++ dajeong/login-project/src/pages/Home.jsx
@@ -15,7 +15,10 @@ export default function Home() {
       {!isLoggedIn && <LoginForm />}
       {isLoggedIn && <p className="home-text">로그인 완료!</p>}
       {isLoggedIn && (
-        <button className="change-button" onClick={() => navigate("/password")}>
+        <button
+          className="home-change-button"
+          onClick={() => navigate("/password")}
+        >
           비밀번호 변경
         </button>
       )}
